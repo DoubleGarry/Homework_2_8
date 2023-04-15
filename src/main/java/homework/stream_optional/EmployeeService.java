30af95dc269c0ecae7e9c16b9e80eb7c5f216a36
Kirill Semenikhin <garry@nextmail.ru>
2023-04-15 19:16:41 +0300

    Start

---
+++ src/main/java/homework/stream_optional/EmployeeService.java
@@ -0,0 +1,11 @@
+package homework.stream_optional;
+
+import java.util.Collection;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+    Employee removeEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+
+    Collection<Employee> findAll();
+}
---
+++ src/main/java/homework/stream_optional/exception/EmployeeStorageIsFullException.java
@@ -0,0 +1,4 @@
+package homework.stream_optional.exception;
+
+public class EmployeeStorageIsFullException extends RuntimeException{
+}
